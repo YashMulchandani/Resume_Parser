1e4cec7715e50b8c454a64867f39b580c21dbee6
Yash Mulchandani <yash.mulchandani123@gmail.com>
2021-11-09 23:40:57 +0530

    Project Completed

--- resume_parser/parser_app/views.py
+++ resume_parser/parser_app/views.py
@@ -36,13 +36,16 @@ def Parser(request):
     if request.method == 'POST':
         customer = Customer.objects.get(user=request.user)
         value = validate(customer)
+
         if value == False:
             return render(request, 'pricing.html', )
+
         Resume.objects.all().delete()
-        file_form = UploadResumeModelForm(request.POST, request.FILES)
+        form = UploadResumeModelForm(request.POST, request.FILES)
         files = request.FILES.getlist('resume')
         resumes_data = []
-        if file_form.is_valid():
+
+        if form.is_valid():
             for file in files:
                 try:
                     # saving the file
@@ -56,26 +59,33 @@ def Parser(request):
                     resume.name = data.get('name')
                     resume.email = data.get('email')
                     resume.mobile_number = data.get('mobile_number')
+
                     if data.get('degree') is not None:
                         resume.education = ', '.join(data.get('degree'))
+
                     else:
                         resume.education = None
                     resume.company_names = data.get('company_names')
                     resume.college_name = data.get('college_name')
                     resume.designation = data.get('designation')
                     resume.total_experience = data.get('total_experience')
+
                     if data.get('skills') is not None:
                         resume.skills = ', '.join(data.get('skills'))
+
                     else:
                         resume.skills = None
+
                     if data.get('experience') is not None:
                         resume.experience = ', '.join(data.get('experience'))
+
                     else:
                         resume.experience = None
                     resume.save()
                 except IntegrityError:
                     messages.warning(request, 'Duplicate resume found:', file.name)
                     return redirect('homepage')
+
             resumes = Resume.objects.all()
             messages.success(request, 'Resumes uploaded!')
             context = {
