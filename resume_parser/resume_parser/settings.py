857479c696c08ee7d9af4201cf97592648c3830e
Yash <yash.mulchandani123@gmail.com>
2022-02-15 23:11:32 +0530

    Changes Done

--- resume_parser/resume_parser/settings.py
+++ resume_parser/resume_parser/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fa^pq*c1#+6_(6jgxxr&2o-9bt$5u_f=aldx0yceaed3#c1b^b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'resumextractor.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
